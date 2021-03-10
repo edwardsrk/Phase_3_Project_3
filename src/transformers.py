8f42e307597795ed9e62bd6799081732479d3812
Svitlana Glibova <s.glibova@gmail.com>
2021-03-10 07:59:50 -0800

    created model pipelines

---
+++ src/transformers.py
@@ -0,0 +1,17 @@
+class CustomTransformer(BaseEstimator, TransformerMixin):
+    
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y = None):
+        return self
+    
+    def transform(self, X, y = None):
+        X_ = X.copy()
+        column_transformer = ColumnTransformer(transformers = [('imputer', SimpleImputer(missing_values = 0.0, strategy = 'most_frequent'), ['construction_year']),                     
+                                                      ('ohe', OneHotEncoder(sparse = False, handle_unknown = 'ignore'), categoricals),
+                                                      ('scaler', StandardScaler(), numericals)], 
+                                                      n_jobs = -1)
+        X_new = column_transformer.fit_transform(X_)
+        return X_new
+     
