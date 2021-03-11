8a7d90808ad86b11ae7821e1c84b32b1cd94c567
Svitlana Glibova <s.glibova@gmail.com>
2021-03-10 21:01:05 -0800

    added py files

--- src/transformers.py
+++ src/transformers.py
@@ -1,4 +1,8 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+
 class CustomTransformer(BaseEstimator, TransformerMixin):
+    
     
     def __init__(self):
         pass
