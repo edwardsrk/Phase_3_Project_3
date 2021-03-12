e1e8e6449ad0f1e56d7f0da43b8c7bfe78b0f996
Rachel <rachelke411@gmail.com>
2021-03-11 16:56:04 -0800

    xgb tuning

--- src/transformers.py
+++ src/transformers.py
@@ -6,21 +6,21 @@ from sklearn.base import BaseEstimator, TransformerMixin
 numericals = ['amount_tsh', 'longitude', 'latitude', 'construction_year']
 categoricals = ['region_code', 'scheme_management', 'quality_group', 'quantity_group', 'source', 'extraction_type_class', 'waterpoint_type']
 
+
 class CustomTransformer(BaseEstimator, TransformerMixin):
-    
-    
     def __init__(self):
-        pass
-    
-    def fit(self, X, y = None):
-        return self
-    
-    def transform(self, X, y = None):
-        X_ = X.copy()
-        column_transformer = ColumnTransformer(transformers = [('imputer', SimpleImputer(missing_values = 0.0, strategy = 'most_frequent'), ['construction_year']),                     
-                                                      ('ohe', OneHotEncoder(sparse = False, handle_unknown = 'ignore'), categoricals),
-                                                      ('scaler', StandardScaler(), numericals)], 
+        self.column_trans = None
+#        self.ohe_col_names = None
+    def fit(self, X_t, y_t = None):
+        X_ = X_t.copy()
+        column_transformer = ColumnTransformer(transformers = [('imputer', SimpleImputer(missing_values = 0.0, strategy = 'most_frequent'), ['construction_year']),
+                                                      ('ohe', OneHotEncoder(sparse = False, handle_unknown = 'ignore'), categoricals)],
                                                       n_jobs = -1)
-        X_new = column_transformer.fit_transform(X_)
-        return X_new
+        column_transformer = column_transformer.fit(X_)
+#        self.ohe_col_names = column_transformer.named_transformers_['ohe'].categories_[0]
+        self.column_trans = column_transformer
+        return column_transformer
+    def transform(self, X, y = None):
+        X_ = self.column_trans.transform(X)
+        return X_
      
